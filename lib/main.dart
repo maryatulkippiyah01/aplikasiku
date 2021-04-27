6ce85277e67d862fe76ba6a564595c41a777f85f
maryatulkippiyah <maryatulkippiyah01@gmail.com>
2021-04-27 14:37:06 +0700

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+// void main() -> runApp(MyApp());
+
+void main() {
+  runApp(new MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("Aplikasi Hello World"),
+        ),
+        body: Text("Hello World"),
+      ),
+    );
+  }
+}
